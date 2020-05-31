558e419ba98dc14b915ecb04143368766320aef1
mean_ideal <60354713+d9249@users.noreply.github.com>
2020-05-31 14:12:53 +0900

    Initial commit

---
+++ CRUD/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path, include
+import base.views
+import login.views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', base.views.base, name="base"),
+    path('login/',include('login.urls')),
+    path('createboard/',include('createboard.urls')),
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
---
+++ createboard/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Board(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Img_board(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
