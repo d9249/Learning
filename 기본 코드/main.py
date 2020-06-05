fd8ae5c7e3670af071f2a4ca8bd57a1e128eaeec
mean_ideal <60354713+d9249@users.noreply.github.com>
2020-06-06 03:04:02 +0900

    78.67%, kernel_initializer="glorot_uniform"

--- 기본 코드/main.py
+++ 기본 코드/main.py
@@ -87,24 +87,30 @@ class ModelMgr():
 
     def get_model(self):
         model = Sequential()
-        model.add(Conv2D(64, (3, 3), padding='same', input_shape=self.x_train.shape[1:]))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        model.add(Conv2D(64, (3, 3), padding='same'))
+        model.add(Conv2D(64, (3, 3), padding='same', kernel_initializer="glorot_uniform", input_shape=self.x_train.shape[1:]))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
-        model.add(Conv2D(64, (3, 3), padding='same'))
+        model.add(Conv2D(64, (3, 3), padding='same', kernel_initializer="glorot_uniform"))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(64, (3, 3), padding='same', kernel_initializer="glorot_uniform"))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(64, (3, 3), padding='same', kernel_initializer="glorot_uniform"))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
         model.add(Flatten())
-        model.add(Dense(64))
+        model.add(Dense(32))
         model.add(Dropout(0.25))
-        model.add(Dense(len(self.target_class)))
+        model.add(Dense(len(self.target_class), kernel_initializer="glorot_uniform"))
         model.add(Activation('softmax'))
 
         return model
