1d78c5a99d99b4c3bb3091ab8782bfdf0aefc39f
d9249 <d9249@kyonggi.ac.kr>
2021-01-04 20:34:05 +0900

    Level 1 Complete.

---
+++ learn-vue-js/vue-cli/src/a.vue
@@ -0,0 +1,32 @@
+<!--
+var appHeader = {
+    template: '<div>header</div>',
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+-->
+
+<!-- vue 입력후 tab하면 기본 템플릿 생성 -->
+<!-- HTML -->
+<template>
+    <div>header</div>
+</template>
+
+<!-- Javascript_인스턴스 옵션 -->
+<script>
+export default {
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+</script>
+
+<!-- CSS -->
+<style>
+
+</style>
