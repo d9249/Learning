67facd572b7a69096b9f67bf23e12cb8538bf6db
d9249 <d9249@kyonggi.ac.kr>
2021-01-15 02:09:48 +0900

    router complete.

---
+++ vue-advanced/vue-news/src/routes/index.js
@@ -0,0 +1,40 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import AskView from '../views/AskView.vue'
+import NewsView from '../views/NewsView.vue'
+import JobsView from '../views/JobsView.vue'
+import ItemView from '../views/ItemView.vue'
+import UserView from '../views/UserView.vue'
+
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+    mode: 'history',
+    routes: [{
+            path: '/',
+            redirect: './news'
+        },
+        {
+            // path: url 주소
+            path: '/news',
+            // component: url 주소로 갔을 때 표시될 컴포넌트
+            component: NewsView,
+        },
+        {
+            path: '/ask',
+            component: AskView,
+        },
+        {
+            path: '/jobs',
+            component: JobsView,
+        },
+        {
+            path: '/item',
+            component: ItemView
+        },
+        {
+            path: '/user',
+            component: UserView
+        }
+    ]
+});
