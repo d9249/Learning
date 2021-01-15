8cd7554e420f88717579c703b046a9a5619fa386
d9249 <d9249@kyonggi.ac.kr>
2021-01-15 20:05:48 +0900

    pages complete.

--- vue-advanced/vue-news/src/views/JobsView.vue
+++ vue-advanced/vue-news/src/views/JobsView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div v-for="item in this.$store.state.jobs">{{ item.title }}</div>
+    <p v-for="item in this.$store.state.jobs">
+      <a v-bind:href="item.url">{{ item.title }}</a>
+      <small>{{ item.time_ago }}, {{ item.domain }}</small>
+    </p>
   </div>
 </template>
 
