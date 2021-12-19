7b5eab0aa1a1f59d0d6ce138b9115a5630a9e48b
sangzun-han <hsjun1996@naver.com>
2021-12-19 16:30:06 +0900

    category

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from .import views
+from .import views2
 
 urlpatterns = [
     path('', views.index, name='index'),
     # ---------
+    path('category', views2.category, name="category")
 ]
--- main/views2.py
+++ main/views2.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 
+def category(request):
+  return render(request, 'category.html')
