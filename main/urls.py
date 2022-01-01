e02003c579ddbde67baacdf1d8857638e3cddf4d
sangzun-han <hsjun1996@naver.com>
2022-01-01 22:25:02 +0900

    fileUplad

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     # ---------
     path('category', views2.category, name="category"),
-    path('createCategory', views2.createCategory, name="createCategory")
-    path('categoryDetail', views2.categoryDetail, name="categoryDetail")
+    path('createCategory', views2.createCategory, name="createCategory"),
+    path('categoryDetail', views2.categoryDetail, name="categoryDetail"),
+    path('fileUpload', views2.fileUpload, name="fileUplad"),
 ]
--- main/views2.py
+++ main/views2.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 
 def category(request):
-  return render(request, 'category.html')
+    return render(request, 'category.html')
+
 
 def createCategory(request):
-  return render(request, 'newDataForm.html')
+    return render(request, 'newDataForm.html')
 
 
 def categoryDetail(request):
     return render(request, 'categoryDetail.html')
+
+
+def fileUpload(request):
+    return render(request, 'fileUpload.html')
