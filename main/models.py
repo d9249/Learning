0508745295588af712ac67a49e64edc7a4315ede
HanMinSS <gksals6464@naver.com>
2022-01-01 23:32:35 +0900

    #3 ✅  ADD Category Model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+  facilityName = models.CharField(null=True, max_length=255)  # 시설물 명
+  facilityNo = models.CharField(null=True, max_length=255)# 시설물 번호
+  adress = models.CharField(null=True, max_length=255)# 시설물 주소
+  completionDate =  models.DateField(null=True)# 준공일자
+  landArea = models.CharField(null=True, max_length=255)# 대지면적
+  buildingArea = models.CharField(null=True, max_length=255)# 건축면적
+  totalBuildingArea = models.CharField(null=True, max_length=255)# 건축연면적
+  highestHeight = models.CharField(null=True, max_length=255)# 최고높이
+  Usage = models.CharField(null=True, max_length=255)# 용도
+  facilityStructure = models.CharField(null=True, max_length=255)# 시설물 구조
+  structuralForm = models.CharField(null=True, max_length=255)# 구조형식 영어
+  amenities = models.CharField(null=True, max_length=255)# 부대시설
+  floors = models.CharField(null=True, max_length=255)# 층별
+  grade = models.CharField(null=True, max_length=255)# 전차안전등급
+  testResults = models.CharField(null=True, max_length=255)# 점검결과
+  plus = models.CharField(null=True, max_length=255)# 규모 및 추가 사항
+  def __str__(self):
+        return str(self.id)
