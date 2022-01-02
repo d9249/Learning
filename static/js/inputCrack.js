e080f8a3667fb52919c638a6f0f95442e26ef74c
sangzun-han <hsjun1996@naver.com>
2022-01-02 10:22:43 +0900

    #4 파일업로드시 사진출력

---
+++ static/js/inputCrack.js
@@ -0,0 +1,55 @@
+const fileTypes = [
+  "image/apng",
+  "image/bmp",
+  "image/gif",
+  "image/jpeg",
+  "image/pjpeg",
+  "image/png",
+  "image/svg+xml",
+  "image/tiff",
+  "image/webp",
+  "image/x-icon",
+];
+
+const input = document.querySelector(".crack-input");
+const preview = document.querySelector(".preview");
+const blankImage = document.querySelector(".blank-img");
+
+input.addEventListener("change", updateImageDisplay);
+
+function validFileType(file) {
+  return fileTypes.includes(file.type);
+}
+
+function updateImageDisplay() {
+  while (preview.firstChild) {
+    preview.removeChild(preview.firstChild);
+  }
+
+  const curFiles = input.files;
+  if (curFiles.lenght === 0) {
+    const para = document.createElement("p");
+    para.textContent = "선택한 파일을 업로드 할 수 없습니다.";
+    preview.appendChild(para);
+  } else {
+    const list = document.createElement("ol");
+    preview.appendChild(list);
+
+    for (const file of curFiles) {
+      const listItem = document.createElement("li");
+      const para = document.createElement("p");
+
+      if (validFileType(file)) {
+        const image = document.createElement("img");
+        image.src = URL.createObjectURL(file);
+        listItem.appendChild(image);
+        blankImage.style.display = "none";
+      } else {
+        para.textContent = `File name ${file.name} : 파일의 형식이 옳지 않습니다.`;
+        listItem.appendChild(para);
+      }
+
+      list.appendChild(listItem);
+    }
+  }
+}
