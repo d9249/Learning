8b9fb7650fbc3fde423e09f1a396e3f8de6472a5
iDeal <60354713+d9249@users.noreply.github.com>
2022-02-10 21:49:20 +0900

    #24 Page separation is complete

--- main/views.py
+++ main/views.py
@@ -97,6 +97,11 @@ def categoryDetail(request, pk):
     crack = Crack.objects.filter(category=category)
     return render(request, 'categoryDetail.html',{'obj':category,'crack':crack})
 
+def buildingDetail(request, pk):
+    category = Category.objects.get(pk=pk)
+    crack = Crack.objects.filter(category=category)
+    return render(request, 'buildingDetail.html',{'obj':category,'crack':crack})
+
 
 def flatting(request, pk):
     crack = CrackObj.objects.get(pk=pk)
